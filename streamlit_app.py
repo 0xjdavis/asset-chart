b447d1517592858d5e8adfdff12625428414d6a5
J. Davis <98430977+0xjdavis@users.noreply.github.com>
2024-08-15 21:56:34 -0700

    Initial commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,12 +13,28 @@ def space(num_lines=1):
     for _ in range(num_lines):
         st.write("")
 
+st.sidebar.header("About App")
+st.sidebar.write("This is a zero-shot text-to-image generation chatbot using OpenAI DALL-E 3 model by 0xjdavis")
 
-st.set_page_config(layout="centered", page_icon="💬", page_title="Commenting app")
+# Calendly
+st.sidebar.markdown("""
+    <hr />
+    <center>
+    <div style="border-radius:8px;padding:8px;background:#fff";width:100%;">
+    <img src="https://avatars.githubusercontent.com/u/98430977" alt="Oxjdavis" height="100" width="100" border="0" style="border-radius:50%"/>
+    <br />
+    <span style="height:12px;width:12px;background-color:#77e0b5;border-radius:50%;display:inline-block;"></span> <b>I'm available for new projects!</b><br />
+    <a href="https://calendly.com/0xjavis" target="_blank"><button style="background:#126ff3;color:#fff;border: 1px #126ff3 solid;border-radius:8px;padding:8px 16px;margin:10px 0">Schedule a call</button></a><br />
+    </div>
+    </center>
+    <br />
+""", unsafe_allow_html=True)
 
-# Data visualisation part
+# Copyright
+st.sidebar.caption("©️ Copyright 2024 J. Davis")
+    
 
-st.title("💬 Commenting app")
+st.title("Asset Chart with Comments")
 
 source = data.stocks()
 all_symbols = source.symbol.unique()
@@ -32,15 +48,13 @@ st.altair_chart(chart, use_container_width=True)
 
 space(2)
 
-# Comments part
-
+# COMMENTS
 conn = db.connect()
 comments = db.collect(conn)
 
-with st.expander("💬 Open comments"):
+with st.expander("View Comments"):
 
     # Show comments
-
     st.write("**Comments:**")
 
     for index, entry in enumerate(comments.itertuples()):
@@ -54,7 +68,6 @@ with st.expander("💬 Open comments"):
     space(2)
 
     # Insert comment
-
     st.write("**Add your own comment:**")
     form = st.form("comment")
     name = form.text_input("Name")
