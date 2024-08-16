093dd4271701b8cf21a62f6b60f188cdf9d7ee8d
J. Davis <98430977+0xjdavis@users.noreply.github.com>
2024-08-16 12:13:06 -0700

    Initial commit

--- utils/db.py
+++ utils/db.py
@@ -11,7 +11,7 @@ from googleapiclient.http import HttpRequest
 socket.setdefaulttimeout(15 * 60)
 
 SCOPE = "https://www.googleapis.com/auth/spreadsheets"
-SPREADSHEET_ID = "1rkMVLvh3JrBq_tbi4Ho0qjCDAP3vYdNuWOEjYpkJLNU"
+SPREADSHEET_ID = "1f7iVHU1tRS0LP9KWRafT0cHN_v02kFo58lOf8eFD2DU"
 SHEET_NAME = "Database"
 GSHEET_URL = f"https://docs.google.com/spreadsheets/d/{SPREADSHEET_ID}"
 
