fef3f46ec39d69cfa004db44525bf797a1246d24
J. Davis <98430977+0xjdavis@users.noreply.github.com>
2024-08-16 14:14:50 -0700

    Initial commit

--- utils/crypto.py
+++ utils/crypto.py
@@ -6,7 +6,7 @@ from datetime import datetime, timedelta
 def get_crypto_historical_data(crypto_id, days):
     coingecko_api_key = st.secrets["coingecko_api_key"]
     
-    url = f'https://api.coingecko.com/api/v3/coins/{crypto_id}/market_chart?vs_currency=usd&days=30&interval=daily&x_cg_demo_api_key={coingecko_api_key}'
+    url = f'https://api.coingecko.com/api/v3/coins/{crypto_id}/market_chart?vs_currency=usd&days=365&interval=daily&x_cg_demo_api_key={coingecko_api_key}'
     parameters = {
         'vs_currency': 'usd',
         'days': days,
@@ -32,6 +32,6 @@ crypto_data = {
 # Save the data to a JSON file
 output_file = 'crypto_price_history.json'
 with open(output_file, 'w') as file:
-    json.dump(crypto_data, file, indent=4)
+    json.dump(crypto_data.prices[], file, indent=4)
 
 print(f"Crypto price history saved to {output_file}")
